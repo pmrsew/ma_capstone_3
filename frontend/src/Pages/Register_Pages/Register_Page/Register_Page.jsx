1f5447268ea982e47d47b2814a01dfd8277ea59c
Paige <paige0011@gmail.com>
2022-10-29 19:07:23 -0500

    reduced code, made changes to css

--- frontend/src/Pages/Register_Pages/Register_Page/Register_Page.jsx
+++ frontend/src/Pages/Register_Pages/Register_Page/Register_Page.jsx
@@ -1,42 +1,32 @@
 import React from 'react'
-import ReactDOM from 'react-dom/client'
 import './Register_Page.css'
 import { Link } from 'react-router-dom'
 
-/*components*/ 
+/*components*/
 import Button_Style_One from '../../../Components/Button.Components/Button_Style_One/Button_Style_One'
 import Brand_Image from '../../../Components/Image.Components/Brand_image/Brand_Image'
 import Text_Banner from '../../../Components/Banner.Component/Text_Banner/Text_Banner'
-import Help_Button from '../../../Components/Button.Components/Help_Button_One/Help_Button_One'
-import Register_Banner from '../../../Components/Banner.Component/Register_Banner/Register_Banner'
 /*components*/
 
 export default function OpeningPage() {
-    return (
-      <div className='Register_Page'>
-        <div className='Register_page_Banner'>
-        <Register_Banner />
-        </div>
+  return (
+    <div className='Register_Page'>
+      <h1 className='Register_Banner'>
+        Meal Planner
+      </h1>
 
-        <div className='Register_page_Icon'>
-          <Brand_Image/>
-        </div>
+      <Brand_Image locationClassName='Register_page_Icon' />
 
-        <div className='Register_page_Welcome_Banner'>
-          <Text_Banner text='This is where the description will got for the welcome page.'/>
-        </div>
+      <Text_Banner text='Welcome to CARPs Meal Planner app! Login if you have an existing account or sign-up for access.' />
 
-        <div className='Register_page_Buttons'>
-          <Link to="/login">
-            <Button_Style_One buttonName="Login"/>
-          </Link>
-          <br/>
-          <Link to="/sign-up">
-            <Button_Style_One buttonName="Sign-Up"/>
-          </Link>
-          <br/>
-          <Help_Button buttonName="Help?"/>
-        </div>
+      <div className='Register_page_Buttons'>
+        <Link to="/login">
+          <Button_Style_One buttonName="Login" />
+        </Link>
+        <Link to="/sign-up">
+          <Button_Style_One buttonName="Sign-Up" />
+        </Link>
       </div>
-    )
-  }
+    </div>
+  )
+}
