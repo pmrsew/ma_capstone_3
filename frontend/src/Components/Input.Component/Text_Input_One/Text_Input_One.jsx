6da29a54f37993e66e07398be85c6e961e765e78
Paige <paige0011@gmail.com>
2022-11-03 14:24:26 -0500

    css formatting edit, mainly in login page

--- frontend/src/Components/Input.Component/Text_Input_One/Text_Input_One.jsx
+++ frontend/src/Components/Input.Component/Text_Input_One/Text_Input_One.jsx
@@ -2,16 +2,16 @@ import React from "react";
 import './Text_Input_One.css'
 
 export default function Text_Input_One(props){
-    const {lableClassName, name, type, id,  placeholder} = props;
+    const {labelClassName, name, type, id,  placeholder} = props;
     
     return(
-        <div className="Input_Body">
-            <label className={lableClassName}>{name}</label>
+        <div className="text-input-one">
+            <label className={labelClassName}>{name}</label>
             <input
             type={type}
             id={id}
             name={name}
-            className="text_input"
+            className="text-input-one_input"
             placeholder={placeholder}
             onChange={(event) => props.onChange(event.target.value)}
             required
--- frontend/src/Pages/Register_Pages/Login_Page/Login_Page.jsx
+++ frontend/src/Pages/Register_Pages/Login_Page/Login_Page.jsx
@@ -1,6 +1,5 @@
 import "./Login_Page.css";
 import React, { useState } from "react";
-import ReactDOM from "react-dom/client";
 import { useNavigate } from "react-router-dom";
 /*redux*/
 import { useDispatch } from "react-redux";
@@ -11,8 +10,6 @@ import { baseUrl } from "../../../api/baseUrl";
 import axios from "axios";
 /*components*/
 import Button_Style_One from '../../../Components/Button.Components/Button_Style_One/Button_Style_One'
-import Help_Button from "../../../Components/Button.Components/Help_Button_One/Help_Button_One";
-import Forgot_Button from "../../../Components/Button.Components/Help_Button_Two/Help_Button_Two";
 import Brand_Image from "../../../Components/Image.Components/Brand_image/Brand_Image";
 import Text_Input_One from "../../../Components/Input.Component/Text_Input_One/Text_Input_One";
 
@@ -37,36 +34,25 @@ export default function LoginPage(props) {
 
   return (
     <div className="login-page">
-      
-      <div className="Login_Banner">
-        <h1 className="login-header">Welcome Back {username}!</h1>
-      </div>
+      <h1 className="login-page_header">Welcome Back {username}!</h1>
 
-      <div className="Login_Icon">
-        <Brand_Image />
-      </div>
+      <Brand_Image locationClassName="login-page_icon" />
       
-      <div className="Login_inputs">
-        <Text_Input_One lableClassName="Username-label" name="Username"
+      <div className="login-page_inputs">
+        <Text_Input_One labelClassName="username-label" name="Username"
           id="username" type="text" inputClassName=""
           placeholder="Username"
           onChange={(value) => setUsername(value)}
           required/>
           <br/>
-        <Text_Input_One lableClassName="Password-label" name="Password" 
+        <Text_Input_One labelClassName="password-label" name="Password" 
           id="password" type="password" inputClassName=""
           placeholder="Password" 
           onChange={(value) => setPassword(value)}
           required/>
       </div>
-      <br/>
-      <div className="Login_Buttons">
+      <div className="login-page_buttons">
         <Button_Style_One buttonName="Login" onClickHandler={handleSubmit}/>
-        <br/>
-        <Forgot_Button buttonName="Forgot Password" />
-      </div>
-      <div className="Login_Help">
-        <Help_Button buttonName="Help?" />
       </div>
     </div>
   );
